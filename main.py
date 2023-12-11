0643699eca774205f12fae65ebfe969555e22aa4
Danielle <61091954+DaniYell1988@users.noreply.github.com>
2023-12-10 20:51:30 -0500

    Add files via upload
    First horoscope code may update later

---
+++ main.py
@@ -0,0 +1,27 @@
+Zodiac = input("What is your zodiac sign?")
+if Zodiac == "Aries" or Zodiac == "aries":
+    print("With all the adrenaline seeking activities you have planned, make sure your life insurance is up to date.")
+elif Zodiac == "Taurus" or Zodiac == "taurus":
+    print("Read this out loud. We all know you love hearing the sound of your own voice.")
+elif Zodiac == "Gemini" or Zodiac == "gemini":
+    print("confused about your life choices much?")
+elif Zodiac == "Cancer" or Zodiac == "cancer":
+    print("You must have typed the wrong zodiac in, we all know Cancers don't play games.")
+elif Zodiac == "Leo" or Zodiac == "leo":
+    print("Hey did you need help? Oh sorry I forgot I was asking a Leo...you NEVER need help.")
+elif Zodiac == "Virgo" or Zodiac == "virgo":
+    print("Why bother asking someone else to do it? No one is as precise as you, just do it yourself.")
+elif Zodiac == "Libra" or Zodiac == "libra":
+    print("Stop flirting! yeah you! Literally everyone you see, you need to calm down.")
+elif Zodiac == "Scorpio" or Zodiac == "scorpio":
+    print("You know everybody's business then tell everyone you mind your own. Sure... you keep believing that.")
+elif Zodiac == "Sagittarius" or Zodiac == "sagittarius":
+    print("What do you have planned later? HA HA HA That's A joke cause you never make plans.")
+elif Zodiac == "Capricorn" or Zodiac == "capricorn":
+    print("What are you doing on here? Shouldn't you be overworking yourself some where?")
+elif Zodiac == "Aquarius" or Zodiac == "aquarius":
+    print("I'm a computer and still don't know as much as you. How do you know it all?")
+elif Zodiac == "Pisces" or Zodiac == "pisces":
+    print("I see you pretending to be innocent and talking about that fairy tale life then at home you are a freak.")
+else:
+    print("Okay, maybe some other time")
